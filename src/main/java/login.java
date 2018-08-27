a22937e8d839761edfc139eb7a5820a502aa22bd
darkness <no@mail.here>
2018-08-27 22:50:45 +0400

    tomcat connection pool

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -1,11 +1,31 @@
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class login extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        InitialContext initContext= null;
+        try {
+            initContext = new InitialContext();
+            DataSource ds = (DataSource) initContext.lookup("java:comp/env/jdbc/appname");
+            Connection conn = ds.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
 //        super.doPost(httpServletRequest, httpServletResponse);
@@ -23,7 +43,24 @@ public class login extends HttpServlet {
 
         String name = httpServletRequest.getParameter("name");
         String password = httpServletRequest.getParameter("password");
-        out.println(name + " " + password);
+//        out.println(name + " " + password);
+
+        InitialContext initContext= null;
+        try {
+            initContext = new InitialContext();
+            DataSource ds = (DataSource) initContext.lookup("java:comp/env/jdbc/appname");
+            Connection con = ds.getConnection();
+
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from example");
+
+            while(rs.next())
+                out.println(rs.getInt(1)+"  "+rs.getString(2));
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
